da5d92d86335b22bfd52f157cce21868126841dd
penina wanjiru <penina.wanjiru@student.moringaschool.com>
2022-06-07 18:23:20 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+const distanceFromHqInBlocks=(value) =>{
+    if(value > 42) {
+        return value - 42;
+} else {return 42 - value;
+}
+}
+
+const distanceFromHqInFeet = (value) => {
+    const result = distanceFromHqInBlocks(value);
+    return result * 264;
+ }
+
+ const distanceTravelledInFeet = (startingPoint, destination) => {
+    if(startingPoint > destination) {
+       return (startingPoint - destination) * 264;
+    } else {
+       return (destination - startingPoint) * 264;
+    }
+ }
+
+ const calculatesFarePrice = (startingPoint, destination) => {
+    const distance = distanceTravelledInFeet(startingPoint, destination);
+    if(distance <= 400) {
+       return 0;
+    } else if(distance > 400 && distance <= 2000) {
+       return (distance - 400) * 0.02;
+    } else if(distance > 2000 && distance <= 2500) {
+       return 25;
+    } else {
+       return 'cannot travel that far';
+    }
+ }
+
