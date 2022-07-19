2c656e1800b19297e67a999fb4df77239701780b
wanjirugachunji <wanjirugachunji@gmail.com>
2022-07-20 00:11:46 +0300

    commit tree 3def7edf7a47724e370ad1043f907e3864ec62e1

--- index.js
+++ index.js
@@ -5,29 +5,3 @@ const distanceFromHqInBlocks=(value) =>{
 }
 }
 
-const distanceFromHqInFeet = (value) => {
-    const result = distanceFromHqInBlocks(value);
-    return result * 264;
- }
-
- const distanceTravelledInFeet = (startingPoint, destination) => {
-    if(startingPoint > destination) {
-       return (startingPoint - destination) * 264;
-    } else {
-       return (destination - startingPoint) * 264;
-    }
- }
-
- const calculatesFarePrice = (startingPoint, destination) => {
-    const distance = distanceTravelledInFeet(startingPoint, destination);
-    if(distance <= 400) {
-       return 0;
-    } else if(distance > 400 && distance <= 2000) {
-       return (distance - 400) * 0.02;
-    } else if(distance > 2000 && distance <= 2500) {
-       return 25;
-    } else {
-       return 'cannot travel that far';
-    }
- }
-
